47cf241ee880739b4e57950f149277ba4fe0a7cf
dzikrialifa <dzikrialif99@gmail.com>
2019-10-02 22:39:26 +0700

    Templating Admin

---
+++ application/views/template_admin/footer.php
@@ -0,0 +1,23 @@
+<script src="<?= base_url('assets_admin/') ?>bower_components/jquery/dist/jquery.min.js"></script>
+<!-- Bootstrap 3.3.7 -->
+<script src="<?= base_url('assets_admin/') ?>bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+<!-- FastClick -->
+<script src="<?= base_url('assets_admin/') ?>bower_components/fastclick/lib/fastclick.js"></script>
+<!-- AdminLTE App -->
+<script src="<?= base_url('assets_admin/') ?>dist/js/adminlte.min.js"></script>
+<!-- Sparkline -->
+<script src="<?= base_url('assets_admin/') ?>bower_components/jquery-sparkline/dist/jquery.sparkline.min.js"></script>
+<!-- jvectormap  -->
+<script src="<?= base_url('assets_admin/') ?>plugins/jvectormap/jquery-jvectormap-1.2.2.min.js"></script>
+<script src="<?= base_url('assets_admin/') ?>plugins/jvectormap/jquery-jvectormap-world-mill-en.js"></script>
+<!-- SlimScroll -->
+<script src="<?= base_url('assets_admin/') ?>bower_components/jquery-slimscroll/jquery.slimscroll.min.js"></script>
+<!-- ChartJS -->
+<script src="<?= base_url('assets_admin/') ?>bower_components/chart.js/Chart.js"></script>
+<!-- AdminLTE dashboard demo (This is only for demo purposes) -->
+<script src="<?= base_url('assets_admin/') ?>dist/js/pages/dashboard2.js"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="<?= base_url('assets_admin/') ?>dist/js/demo.js"></script>
+</body>
+
+</html>
