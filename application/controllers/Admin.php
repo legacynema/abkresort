0302b258b452e0ba90427bc063c8508e094677b6
dzikrialifa <dzikrialif99@gmail.com>
2019-10-03 13:15:11 +0700

    Edit Functions on Controller Admin

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -1,13 +1,47 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Admin extends CI_Controller {
+class Admin extends CI_Controller
+{
 
-	
+	public function __construct()
+	{
+		parent::__construct();
+	}
 	public function index()
 	{
 		$this->load->view('template_admin/header');
 		$this->load->view('template_admin/sidebar');
+		$this->load->view('admin/content');
 		$this->load->view('template_admin/footer');
 	}
+	public function tambah_penginapan()
+	{ }
+	public function tambah_transport()
+	{ }
+	public function tambah_wisata()
+	{ }
+
+	// VIEWS EDIT DATA
+	public function edit_penginapan()
+	{ }
+	public function edit_transport()
+	{ }
+	public function edit_transaksi()
+	{ }
+	public function edit_wisata()
+	{ }
+
+	// VIEWS LIST USER
+	public function user_aktif()
+	{ }
+	public function user_nonaktif()
+	{ }
+
+	// VIEWS TRANSAKSI
+	public function transaksi()
+	{ }
+	// VIEWS LAPORAN
+	public function laporan()
+	{ }
 }
