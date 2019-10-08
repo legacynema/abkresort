585f96028860bde37730705dd51d602d28e32ee1
Alfaza Satria <alfazasatria8@gmail.com>
2019-10-08 07:58:33 +0700

    Catalog category wisata done

--- application/views/home_template/home_view.php
+++ application/views/home_template/home_view.php
@@ -33,33 +33,82 @@
   </a>
 </div>
 <br><br>
-<div class="container">
-  <div class="row">
-    <div class="col-md-3">
-      <div class="card" style="width: 18rem;">
-        <img src="<?= base_url('assets/') ?>img/Malang.jpg" class="d-block w-100" width="100%" height="100%">
-        <a href="#" class="btn btn-primary btn-lg btn-block">Malang</a>
-      </div>
+<div class="container indt-container new-hhd-container pb-5 pt-5">
+    <h3 class="mb-3">Destination Highlights</h3>
+
+	<div class="row no-gutters">
+        
+                <a href="#" class="col new-hhd col-high-dest" style="display:block;">
+              		<div class="new-hhd-card" >
+                  <img src="<?= base_url('assets/') ?>img/Malang.jpg" class="d-block w-100" width="100%" height="100%">
+                  		<div class="new-hhd-body">Malang</div>
+               		</div>
+           		</a>
+              
+                <a href="#" class="col new-hhd col-high-dest" style="display:block;">
+              		<div class="new-hhd-card">
+                  <img src="<?= base_url('assets/') ?>img/Surabaya.jpg" class="d-block w-100" width="100%" height="100%">
+                  		<div class="new-hhd-body">Surabaya</div>
+               		</div>
+           		</a>
+              
+                <a href="#" class="col new-hhd col-high-dest" style="display:block;">
+              		<div class="new-hhd-card" >
+                  <img src="<?= base_url('assets/') ?>img/Jogja.jpg" class="d-block w-100" width="100%" height="100%">
+                  		<div class="new-hhd-body">Yogyakarta</div>
+               		</div>
+           		</a>
+              
+                <a href="#" class="col new-hhd col-high-dest" style="display:block;">
+              		<div class="new-hhd-card" >
+                  <img src="<?= base_url('assets/') ?>img/Manado.jpg" class="d-block w-100" width="100%" height="100%">
+                  		<div class="new-hhd-body">Manado</div>
+               		</div>
+           		</a>
+                
+                <a href="#" class="col new-hhd col-high-dest">
+              		<div class="new-hhd-card" >
+                  <img src="<?= base_url('assets/') ?>img/Jombang.jpg" class="d-block w-100" width="100%" height="100%">
+                  		<div class="new-hhd-body">Jombang</div>
+               		</div>
+           		</a>
+                
+                <a href="#" class="col new-hhd col-high-dest">
+              		<div class="new-hhd-card" >
+                  <img src="<?= base_url('assets/') ?>img/Medan.jpg" class="d-block w-100" width="100%" height="100%">
+                  		<div class="new-hhd-body">Medan</div>
+               		</div>
+           		</a>
+                
+                <a href="#" class="col new-hhd col-high-dest">
+              		<div class="new-hhd-card" >
+                  <img src="<?= base_url('assets/') ?>img/Bandung.jpg" class="d-block w-100" width="100%" height="100%">
+                  		<div class="new-hhd-body">Bandung</div>
+               		</div>
+           		</a>
+                
+                <a href="#" class="col new-hhd col-high-dest">
+              		<div class="new-hhd-card" >
+                  <img src="<?= base_url('assets/') ?>img/Jakarta.jpg" class="d-block w-100" width="100%" height="100%">
+                  		<div class="new-hhd-body">Jakarta</div>
+               		</div>
+           		</a>
+                
+                <a href="#" class="col new-hhd col-high-dest">
+              		<div class="new-hhd-card" >
+                  <img src="<?= base_url('assets/') ?>img/Lombok.jpg" class="d-block w-100" width="100%" height="100%">
+                  		<div class="new-hhd-body">Lombok</div>
+               		</div>
+           		</a>
+                
+                <a href="#" class="col new-hhd col-high-dest">
+              		<div class="new-hhd-card" >
+                  <img src="<?= base_url('assets/') ?>img/Bali.jpg" class="d-block w-100" width="100%" height="100%">
+                  		<div class="new-hhd-body">Bali</div>
+               		</div>
+           		</a>
+          
     </div>
-    <div class="col-md-3">
-      <div class="card" style="width: 18rem;">
-        <img src="<?= base_url('assets/') ?>img/Malang.jpg" class="d-block w-100" width="100%" height="100%">
-        <a href="#" class="btn btn-primary btn-lg btn-block">Malang</a>
-      </div>
-    </div>
-    <div class="col-md-3">
-      <div class="card" style="width: 18rem;">
-        <img src="<?= base_url('assets/') ?>img/Malang.jpg" class="d-block w-100" width="100%" height="100%">
-        <a href="#" class="btn btn-primary btn-lg btn-block">Malang</a>
-      </div>
-    </div>
-    <div class="col-md-3">
-      <div class="card" style="width: 18rem;">
-        <img src="<?= base_url('assets/') ?>img/Malang.jpg" class="d-block w-100" width="100%" height="100%">
-        <a href="#" class="btn btn-primary btn-lg btn-block">Malang</a>
-      </div>
-    </div>
-  </div>
 </div>
   
     <!-- Optional JavaScript -->
@@ -69,3 +118,138 @@
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
   </body>
 </html>
+
+<style>
+    .new-hhd-container > .row {
+        margin-left: -8px;
+        margin-right: -8px;
+    }
+    .new-hhd {
+        padding: 10px !important;
+    }
+    .new-hhd-card {
+        height: 230px;
+        box-shadow: 0px 0px 7px rgba(0, 0, 0, 0.25);
+        position:relative;
+        background-repeat:no-repeat;
+        background-size:cover;
+        background-position:center;
+    }
+    .new-hhd-body {
+        width: 100%;
+        background-image: linear-gradient(to bottom, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.5));
+        text-align: center;
+        padding: 15px;
+        color: white;
+        position: absolute;
+        bottom: 0;
+    }
+    .text-dark {
+        color:#343a40 !important;
+    }
+
+    .btn-hh {
+        position: absolute;
+        top: 50%;
+        border-radius: unset;
+        border-bottom-left-radius: 30px;
+        border-top-left-radius: 30px;
+        width: 30px;
+        height: 40px;
+        transform: translate(-100%, -30%);
+        color: #d71984;
+        background-color: white;
+        box-shadow: -3px 0px 4px -1px rgba(0, 0, 0, 0.40);
+    }
+    .btn-hh:focus {
+        box-shadow: -3px 0px 4px -1px rgba(0, 0, 0, 0.40);
+        border:1px solid #cecece;
+    }
+
+    .btn-hh.btn-hh-left {
+        left: 0px;
+    }
+    .btn-hh.btn-hh-right {
+        right: 0px;
+        transform: rotate(180deg) translate(-100%, 30%);
+    }
+    .col-high-dest {
+    	-webkit-box-flex: 0;
+-webkit-flex: 0 0 20%;
+-ms-flex: 0 0 20%;
+flex: 0 0 20%;
+max-width: 20;
+    }
+
+@media(min-width:768px){
+    .show-mobile {
+        display:none;
+    }
+    .show-desktop {
+        display:block;
+    }
+}
+@media(max-width:767px){
+    .show-mobile {
+        display:block;
+    }
+    .show-desktop {
+        display:none;
+    }
+    /*.homepage-highlight-destination {
+        padding-left:15px;
+        padding-right:15px;
+    }*/
+    .new-hhd {
+        width: 50%;
+        flex-basis: unset;
+        display: inline-block;
+        padding: 5px !important;
+    }
+    .new-hhd-container {
+        overflow:hidden;
+        padding-top: 0px !important;
+        padding: 0 15px !important;
+    }
+    .new-hhd-container > .row {
+        /*overflow: auto;
+        white-space: nowrap;
+        display: block;*/
+        margin-left: -5px;
+        margin-right: -5px;
+        margin-bottom:15px;
+    }
+    .new-hhd-arrows {
+        height:0px;
+        position:relative;
+        z-index:1;
+    }
+    .hhd-arrow {
+        height: 50px;
+        width: 50px;
+        line-height: 40px;
+        border-radius: 50%;
+        z-index: 1;
+        position: absolute;
+        top: -23px;
+        padding: 0px;
+        font-size: 24px;
+        text-indent: 7px;
+        color: #d71984;
+        background-color: white;
+    }
+    .hhd-arrow:focus {
+        box-shadow:unset;
+    }
+    .hhd-arrow.arrow-left {
+        left: -25px;
+    }
+    .hhd-arrow.arrow-right {
+        right: -25px;
+        transform: rotate(180deg);
+    }
+    .col-high-dest {
+    	display:none;
+	}
+}
+</style>
