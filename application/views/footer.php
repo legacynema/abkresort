990f44ae540c1d8264d61d5d469d5106887313ed
firmancitranurrahman <firmancitranurrahman@gmail.com>
2019-10-24 12:03:44 +0700

    edit footer

--- application/views/footer.php
+++ application/views/footer.php
@@ -1,4 +1,7 @@
 <link rel="stylesheet" href="<?= base_url('node_modules/') ?>bootstrap/dist/css/app.css">
+<link rel="stylesheet" href="<?= base_url('node_modules/') ?>bootstrap/dist/css/bootstrap.css">
+<!-- <link rel="stylesheet" href="<?= base_url('node_modules/') ?>bootstrap/dist/css/destination.css"> -->
+
 <footer class="foot">
             <p class="text-center">Copyright © 2019 <a href="#" target="_blank">Legacynema</a></p>
         </footer>
