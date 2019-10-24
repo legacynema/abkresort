f44ff238c9b87ada719896e965b1cb1d9ecd4f1f
firmancitranurrahman <firmancitranurrahman@gmail.com>
2019-10-24 12:04:02 +0700

    create view destination

---
+++ application/views/destination_template/destination_view.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <div class="enigma_header_breadcrum_title">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <h1>Destinasi Wisata Di kota Malang</h1>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-2"></div>
+            <div class="col-md-8">
+                <img src="<?= base_url('assets/') ?>img/tugu.jpg"  class="d-block w-100" height="100%"  alt="">
+            </div>
+            <div class="col-md-2"></div>
+    </div>
+    <div class="text-center">
+        <h3>Malang</h3>
+        <p class="t--larger"></p>
+        <p>Malang merupakan kota pilihan wisata yang kaya akan keindahan alam-nya, dan dapat di jadikan tempat liburan yang menyenangkan untuk Anda dengan teman ataupun keluarga. Nikmati pilihan paket menarik hanya di AbkResort Travel </p>
+    </div><hr>
+    <section class="section-block">
+        <div class="block section-head clearfix" style>
+            <h3>Destinasi Di Malang</h3>
+        </div>
+        <div class="block post inset-on-m">
+            <div class="block post__item bzg" style>
+                <h3 class="bzg_c block--small "style>
+                    <a href="#" class="link-black t--larger"style>Paralayang</a>
+                </h3>
+                <div class="bzg_c col-md-4" >
+                    <img src="<?= base_url('assets/') ?>img/paralayangmalang.jpg" alt="">
+                </div>
+                <div class="bzg_c col-md-8">
+                    <div class="block">
+                            Salah satu objek wisata yang menyediakan fasilitas kegiatan yang memicu dan memacu adrenalin. Paralayang Batu Malang bisa membuat anda mengucurkan keringat dingin, bersemangat, berteriak sekaligus berdecak kagum.
+                            Paralayang adalah nama untuk Puncak Gunung Banyak, Kota Batu yang memiliki ketinggian 1.326 mdpl. Tempat ini pada mulanya merupakan tempat yang dijadikan sebagai landasan take-off atlet paralayang.
+                            Lokasi ini dijadikan pusat latihan dan event paralayang se-Malang Raya. Diresmikan pada tahun 2000, bersamaan dengan diadakannya event PON VI Jatim yang ditandai dengan penandatanganan prasasti oleh Ketua Umum Federasi Aero Sport Indonesia Marsekal TNI Hanafi Asnan.
+                            Di Paralayang, para atlet dapat melayang sambil menikmati keindahan alam dari udara. Pesona pemandangan yang dapat dinikmati para olahragawan menjadi sajian yang menakjubkan, sehingga makin banyak para atlet yang menjajal area ini.    
+                   </div>
+                   <div class="block text-right">
+                       <a href="#" class="btn btn--round btn--ghost-red text-up"style >Selengkapnya</a>
+                   </div>
+                </div>
+            </div>
+            <div class="block post__item bzg" style>
+                <h3 class="bzg_c block--small "style>
+                    <a href="#" class="link-black t--larger"style>Kampung Warna Warni </a>
+                </h3>
+                <div class="bzg_c col-md-4" >
+                    <img src="<?= base_url('assets/') ?>img/kampungwarna.jpg" alt="">
+                </div>
+                <div class="bzg_c col-md-8">
+                    <div class="block">
+                            Salah satu objek wisata yang menyediakan fasilitas kegiatan yang memicu dan memacu adrenalin. Paralayang Batu Malang bisa membuat anda mengucurkan keringat dingin, bersemangat, berteriak sekaligus berdecak kagum.
+                            Paralayang adalah nama untuk Puncak Gunung Banyak, Kota Batu yang memiliki ketinggian 1.326 mdpl. Tempat ini pada mulanya merupakan tempat yang dijadikan sebagai landasan take-off atlet paralayang.
+                            Lokasi ini dijadikan pusat latihan dan event paralayang se-Malang Raya. Diresmikan pada tahun 2000, bersamaan dengan diadakannya event PON VI Jatim yang ditandai dengan penandatanganan prasasti oleh Ketua Umum Federasi Aero Sport Indonesia Marsekal TNI Hanafi Asnan.
+                            Di Paralayang, para atlet dapat melayang sambil menikmati keindahan alam dari udara. Pesona pemandangan yang dapat dinikmati para olahragawan menjadi sajian yang menakjubkan, sehingga makin banyak para atlet yang menjajal area ini.    
+                   </div>
+                   <div class="block text-right">
+                       <a href="#" class="btn btn--round btn--ghost-red text-up"style >Selengkapnya</a>
+                   </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</body>
+</html>
