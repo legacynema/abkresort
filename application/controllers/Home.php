b367c1111a16d535b9668e9e8ee871e5571d6727
firmancitranurrahman <firmancitranurrahman@gmail.com>
2019-10-24 12:02:47 +0700

    Menambahkan function

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -37,4 +37,11 @@ class Home extends CI_Controller {
 		$this->load->view('login_register/register');
 		$this->load->view('footer');
 	}
+	public function destination()
+	{
+		$this->load->view('navbar');
+		$this->load->view('destination_template/destination_view');
+		$this->load->view('footer');
+	}
+	
 }
