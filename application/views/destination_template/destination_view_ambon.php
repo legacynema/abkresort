5e5cfa1f2cf54d85e8c8e4003074545bf489abd9
firmancitranurrahman <firmancitranurrahman@gmail.com>
2019-11-19 19:32:53 +0700

    create view destination ambon

---
+++ application/views/destination_template/destination_view_ambon.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+<body>
+    <div class="enigma_header_breadcrum_title">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <h1>Destinasi Wisata Di kota Ambon</h1>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-2"></div>
+            <div class="col-md-8">
+                <img src="<?= base_url('assets/') ?>img/lautmanado.jpg"  class="d-block w-100" height="100%"  alt="">
+            </div>
+            <div class="col-md-2"></div>
+    </div>
+    <div class="text-center">
+        <h3>Manado</h3>
+        <p class="t--larger"></p>
+        <p>Manado adalah ibu kota dari provinsi Sulawesi Utara. Manado seringkali disebut sebagai Menado. Manado terletak di Teluk Manado, dan dikelilingi oleh daerah pegunungan.  Nikmati pilihan paket menarik hanya di AbkResort Travel </p>
+    </div><hr>
+    <section class="section-block">
+        <div class="block section-head clearfix" style>
+            <h3>Destinasi Di Ambon</h3>
+        </div>
+        <div class="block post inset-on-m">
+            <div class="block post__item bzg" style>
+                <h3 class="bzg_c block--small "style>
+                    <a href="#" class="link-black t--larger"style>Air Terjun Kali</a>
+                </h3>
+                <div class="bzg_c" data-col="m4">
+                    <figure class>
+                    <a class="responsive-media media--3-2 fill--lightgrey">
+                    <img src="<?= base_url('assets/') ?>img/airkali.jpg" alt="" class="">
+                    </a> 
+                    </figure>
+                </div>
+                <div class="bzg_c" data-col="m8">
+                    <div class="block">
+                            Air terjun kali merupakan air terjun sangat fenomenal yang dijadikan sebagai objek wisata dan menarik wisatawan asing. 
+                            Air Terjun Kali terletak di Desa Kali, Kecamatan Pineleng, Minahasa, Sulut. Air terjun yang memiliki ketinggian sekitar 
+                            60 meter ini hanya berjarak sekitar 15 kilometer dari Manado, atau dengan waktu tempuh 50 menit menggunakan kendaraan bermotor.
+                   </div>
+                   <div class="block text-right">
+                       <a href="#" class="btn btn--round btn--ghost-red text-up"style >Selengkapnya</a>
+                   </div>
+                </div>
+            </div>
+            <div class="block post__item bzg" style>
+                <h3 class="bzg_c block--small "style>
+                    <a href="#" class="link-black t--larger"style>Pantai Malayang </a>
+                </h3>
+                <div class="bzg_c col-md-4" >
+                    <img src="<?= base_url('assets/') ?>img/malayangbeach.jpg" alt="">
+                </div>
+                <div class="bzg_c col-md-8">
+                    <div class="block">
+                    Pantai Malalayang yang terletak di ujung selatan kota Manado Sulawesi Utara, memiliki banyak hal yang menarik untuk di nikmati. Diantaranya taman laut yang masih alami dengan berbagai jenis koral warna warni dan spesies ikan berbagai jenis. Untuk sampai di pantai ini, dibutuhkan waktu sekitar 10 menit dari pusat kota Manado dengan menggunakan angkutan umum. Jaraknya sekitar 4 kilometer saja. 
+                    Pantai ini menjadi satu-satunya tempat wisata rakyat yang murah meriah, karena aksesnya dekat dan mudah ditempuh.
+                   </div>
+                   <div class="block text-right">
+                       <a href="#" class="btn btn--round btn--ghost-red text-up"style >Selengkapnya</a>
+                   </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</body>
+</html>
