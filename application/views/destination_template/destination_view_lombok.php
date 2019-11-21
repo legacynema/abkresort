8e87dfb86e922f5cfba150c69c8544b33513758e
firmancitranurrahman <firmancitranurrahman@gmail.com>
2019-11-21 15:52:45 +0700

    create destination view lombok

---
+++ application/views/destination_template/destination_view_lombok.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+<body>
+    <div class="enigma_header_breadcrum_title">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <h1>Destinasi Wisata Di kota Lombok</h1>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-2"></div>
+            <div class="col-md-8">
+                <img src="<?= base_url('assets/') ?>img/lombokhd.jpg"  class="d-block w-100" height="100%"  alt="">
+            </div>
+            <div class="col-md-2"></div>
+    </div>
+    <div class="text-center">
+        <h3>Lombok</h3>
+        <p class="t--larger"></p>
+        <p>Lombok adalah sebuah pulau di kepulauan Sunda Kecil atau Nusa Tenggara yang terpisahkan oleh Selat Lombok dari Bali di sebelat barat dan Selat Alas di sebelah timur dari Sumbawa. Pulau ini kurang lebih berbentuk bulat dengan semacam "ekor" di sisi barat daya yang panjangnya kurang lebih 70 km. Luas pulau ini mencapai 5.435 km² menempatkannya pada peringkat 108 dari daftar pulau berdasarkan luasnya di dunia. Kota utama di pulau ini adalah Kota Mataram. Nikmati pilihan paket menarik hanya di AbkResort Travel </p>
+    </div><hr>
+    <section class="section-block">
+        <div class="block section-head clearfix" style>
+            <h3>Destinasi Di Lombok</h3>
+        </div>
+        <div class="block post inset-on-m">
+            <div class="block post__item bzg" style>
+                <h3 class="bzg_c block--small "style>
+                    <a href="#" class="link-black t--larger"style>Pantai Senggigi</a>
+                </h3>
+                <div class="bzg_c" data-col="m4">
+                    <figure class>
+                    <!-- <a class="responsive-media media--3-2 fill--lightgrey"> -->
+                    <img src="<?= base_url('assets/') ?>img/senggigibeach.jpg" alt="" class="">
+                    </a> 
+                    </figure>
+                </div>
+                <div class="bzg_c" data-col="m8">
+                    <div class="block">
+                    Pantai Senggigi adalah tempat pariwisata yang terkenal di Lombok. Letaknya di sebelah barat pesisir Pulau Lombok. Pantai Senggigi memang tidak sebesar Pantai Kuta di Bali, tetapi seketika kita berada di sini akan merasa seperti berada di Pantai Kuta, Bali.
+                    Memasuki area pantai Senggigi, wisatawan seta merta disapa oleh lembutnya angin semilir yang menenangkan. Pesisir pantainya masih asri, 
+                    walaupun masih ada sampah dedaunan yang masih berserakan karena jarang dibersihkan.
+                    Pemandangan bawah lautnya sangat indah, dan wisatawan bisa melakukan selam permukaan (snorkeling) sepuasnya karena ombaknya tidak terlalu besar. 
+                    Terumbu karangnya menjulang ketengah menyebabkan ombak besarnya pecah di tengah. 
+                    Tersedia juga hotel-hotel dengan harga yang bervariasi, dari yang mahal sampai hotel yang berharga ekonomis.
+                   </div>
+                   <div class="block text-right">
+                       <a href="#" class="btn btn--round btn--ghost-red text-up"style >Selengkapnya</a>
+                   </div>
+                </div>
+            </div>
+            <div class="block post__item bzg" style>
+                <h3 class="bzg_c block--small "style>
+                    <a href="#" class="link-black t--larger"style>Air terjun Sendang Gile </a>
+                </h3>
+                <div class="bzg_c col-md-4" >
+                    <img src="<?= base_url('assets/') ?>img/sendang.jpg" alt="">
+                </div>
+                <div class="bzg_c col-md-8">
+                    <div class="block">
+                    Air terjun Sendang Gile adalah air terjun yang terletak di desa Senaru, kecamatan Bayan, kabupaten Lombok Utara, propinsi Nusa Tenggara Barat. 
+                    Lebih tepatnya berada di kawasan desa Senaru, 2 kilometer dari desa Bayan.Objek wisata ini masih berada dalam kawasan Taman Nasional Gunung Rinjani.[1] Ini disebabkan karena air terjun ini merupakan pintu masuk pendakian menuju gunung Rinjani.[2] Selain itu, ternyata air di Sendang Gile berasal dari mata air di Gunung Rinjani yang sangat sejuk dan alami.[2]
+                    Untuk menuju ke tempat ini, para pengunjung harus melalui dua ratusan anak tangga yang mencapai ketinggian 40 meter, dan jembatan berlubang melalui sebuah lembah yang membutuhkan waktu penjelajahan sekitar 15 menit.[1] Perjalanan alternatif juga bisa dilakukan dengan cara menyelusuri pinggir lembah yang agak curam dan mengikuti saluran irigasi serta menyeberangi jembatan yang terbuat dari rotan.[3]
+                   </div>
+                   <div class="block text-right">
+                       <a href="#" class="btn btn--round btn--ghost-red text-up"style >Selengkapnya</a>
+                   </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</body>
+</html>
