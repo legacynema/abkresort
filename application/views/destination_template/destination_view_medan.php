a0d0f839d31436f907f5f0fddaa0d9b9a7701149
firmancitranurrahman <firmancitranurrahman@gmail.com>
2019-11-21 14:13:37 +0700

    create destination view medan

---
+++ application/views/destination_template/destination_view_medan.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+<body>
+    <div class="enigma_header_breadcrum_title">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <h1>Destinasi Wisata Di kota Medan</h1>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-2"></div>
+            <div class="col-md-8">
+                <img src="<?= base_url('assets/') ?>img/gadangmedan.jpg"  class="d-block w-100" height="100%"  alt="">
+            </div>
+            <div class="col-md-2"></div>
+    </div>
+    <div class="text-center">
+        <h3>Medan</h3>
+        <p class="t--larger"></p>
+        <p>Ambon merupakan pusat pelabuhan, pariwisata, dan pendidikan bagi wilayah Kepulauan Maluku.Ambon disebut sebagai pusat pariwisata karena menawarkan beragam jenis wisata, mulai dari alam, budaya, bahari, hingga kuliner. Nikmati pilihan paket menarik hanya di AbkResort Travel </p>
+    </div><hr>
+    <section class="section-block">
+        <div class="block section-head clearfix" style>
+            <h3>Destinasi Di Medan</h3>
+        </div>
+        <div class="block post inset-on-m">
+            <div class="block post__item bzg" style>
+                <h3 class="bzg_c block--small "style>
+                    <a href="#" class="link-black t--larger"style>Istana Maimun</a>
+                </h3>
+                <div class="bzg_c" data-col="m4">
+                    <figure class>
+                    <!-- <a class="responsive-media media--3-2 fill--lightgrey"> -->
+                    <img src="<?= base_url('assets/') ?>img/maimun.jpg" alt="" class="">
+                    </a> 
+                    </figure>
+                </div>
+                <div class="bzg_c" data-col="m8">
+                    <div class="block">
+                    Istana Maimun adalah istana Kesultanan Deli yang merupakan salah satu ikon kota Medan, Sumatera Utara, terletak di Jalan Brigadir Jenderal Katamso, Kelurahan Sukaraja, Kecamatan Medan Maimun.
+                    Didesain oleh arsitek Italia dan dibangun oleh Sultan Deli, Sultan Mahmud Al Rasyid. Pembangunan istana ini dimulai dari 26 Agustus 1888 dan selesai pada 18 Mei 1891. Istana Maimun memiliki luas sebesar 2.772 m2 dan 30 ruangan. Istana Maimun terdiri dari 2 lantai dan memiliki 3 bagian yaitu bangunan induk, bangunan sayap kiri dan bangunan sayap kanan. 
+                    Bangunan istana ini menghadap ke utara dan pada sisi depan terdapat bangunan Masjid Al-Mashun atau yang lebih dikenal dengan sebutan Masjid Raya Medan.
+                   </div>
+                   <div class="block text-right">
+                       <a href="#" class="btn btn--round btn--ghost-red text-up"style >Selengkapnya</a>
+                   </div>
+                </div>
+            </div>
+            <div class="block post__item bzg" style>
+                <h3 class="bzg_c block--small "style>
+                    <a href="#" class="link-black t--larger"style>The Le Hu Garden Medan</a>
+                </h3>
+                <div class="bzg_c col-md-4" >
+                    <img src="<?= base_url('assets/') ?>img/hugarden.jpg" alt="">
+                </div>
+                <div class="bzg_c col-md-8">
+                    <div class="block">
+                        Dengan mengusung konsep kekinian yang berbeda, T-Garden Resort menyajikan nuansa Bali yang sangat kental, sangat bagus untuk kamu jadikan lokasi selfie yang instagramable, terutama bagi kamu yang berburu spot foto romantis.
+                        Di tempat ini disediakan fasilitas outbound yang cukup lengkap seperti Flying Fox dan ATV.  Ada juga camping ground dan cafe dengan harga yang cukup terjangkau.
+                        Lokasi: Jl Deli Tua – Pancur Batu atau sekitar 10 menit perjalanan dari Kebun Binatang Medan
+                   </div>
+                   <div class="block text-right">
+                       <a href="#" class="btn btn--round btn--ghost-red text-up"style >Selengkapnya</a>
+                   </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</body>
+</html>
