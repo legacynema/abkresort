8009eb1a37ad194fb050c2eaabb4115fb53cc0ed
KhosyAkmal <akmalkhosy@gmail.com>
2019-12-15 14:25:36 +0700

    merapikan coding

--- application/views/admin/editPenginapan.php
+++ application/views/admin/editPenginapan.php
@@ -7,38 +7,38 @@
                 <div class="box-header with-border">
                     <h3 class="box-title">Tambah Penginapan</h3>
                 </div>
-                
+
                 <!-- /.box-header -->
                 <!-- form start -->
-                <form action="<?php echo base_url("Admin/penginapanEdit/$penginapan->id_penginapan")?>"  name="form"  onsubmit="return validateForm()" method="post" enctype="multipart/form-data">
-                <input type="hidden" name="id_penginapan" value="<?php echo $penginapan->id_penginapan?>" />    
+                <form action="<?php echo base_url("Admin/penginapanEdit/$penginapan->id_penginapan") ?>" name="form" onsubmit="return validateForm()" method="post" enctype="multipart/form-data">
+                    <input type="hidden" name="id_penginapan" value="<?php echo $penginapan->id_penginapan ?>" />
                     <div class="box-body">
                         <div class="form-group">
                             <label for="id_kota">Pilih Kota</label>
-                                <select class="form-control" name="id_kota">
-                                    <option value="" disabled selected>Pilih Kota</option>
+                            <select class="form-control" name="id_kota">
+                                <option value="" disabled selected>Pilih Kota</option>
 
-                                    <?php foreach($kota as $kot):?>
-                                                <option value="<?= $kot->id_kota?>" <?php if($penginapan->id_kota == $kot->id_kota ){ echo 'selected'; } ?> > <?= $kot->nama_kota?> </option>
-                                            <?php  endforeach;?>
-                                </select>
+                                <?php foreach ($kota as $kot) : ?>
+                                    <option value="<?= $kot->id_kota ?>" <?php if ($penginapan->id_kota == $kot->id_kota) {echo 'selected';} ?>> <?= $kot->nama_kota ?> </option>
+                                <?php endforeach; ?>
+                            </select>
                         </div>
-                        
+
                         <div class="form-group">
                             <label for="nama_penginapan">Nama Hotel/Villa</label>
-                            <input class="form-control" type="text"  name="nama_penginapan" value="<?php echo $penginapan->nama_penginapan?>">   
+                            <input class="form-control" type="text" name="nama_penginapan" value="<?php echo $penginapan->nama_penginapan ?>">
                         </div>
 
                         <div class="form-group">
                             <label for="jumlah_tamu">Jumlah Tamu</label>
-                            <input type="number" class="form-control" name="jumlah_tamu" value="<?php echo $penginapan->jumlah_tamu?>">
+                            <input type="number" class="form-control" name="jumlah_tamu" value="<?php echo $penginapan->jumlah_tamu ?>">
                         </div>
 
                         <div class="form-group">
                             <label for="harga">Harga *per hari</label>
                             <div class="input-group">
                                 <span class="input-group-addon"><i class="fa fa-money"></i></span>
-                                <input type="text" name="harga" class="form-control" value="<?php echo $penginapan->harga?>">
+                                <input type="text" name="harga" class="form-control" value="<?php echo $penginapan->harga ?>">
                             </div>
                         </div>
 
@@ -46,36 +46,35 @@
                             <label for="foto">Foto *max size 1MB</label>
                             <input type="file" class="form-control" name="foto">
                             <input class="form-control-file" type="hidden" name="old_image" value="<?php echo $penginapan->foto ?>" />
-                            <img src="<?php echo base_url('foto/admin/penginapan/'.$penginapan->foto) ?>" width="64" />
+                            <img src="<?php echo base_url('foto/admin/penginapan/' . $penginapan->foto) ?>" width="64" />
                         </div>
 
                         <script>
-                                function validateForm() {
+                            function validateForm() {
                                 var namaPeng = document.forms["form"]["nama_penginapan"].value;
                                 var kota = document.forms["form"]["id_kota"].value;
                                 var Harga = document.forms["form"]["harga"].value;
                                 var tamu = document.forms["form"]["jumlah_tamu"].value;
 
-                                    if(kota == "" && namaPeng == "" && tamu == "" && Harga == ""){
-                                        namaPeng.innerHTML = "data penginapan harus diisi";
-                                        return false;
-                                    }
-                                    else if (kota == "" ) {
-                                        alert("Kota Harus di Isi");
-                                        return false;
-                                    }else if (namaPeng == ""){
-                                        alert("Nama Harus di Isi");
-                                        return false;
-                                    }else if (tamu == ""){
-                                        alert("Jumlah Tamu Harus di Isi");
-                                        return false;
-                                    }else if (Harga == ""){
-                                        alert("Harga Harus di Isi");
-                                        return false;
-                                    }
+                                if (kota == "" && namaPeng == "" && tamu == "" && Harga == "") {
+                                    namaPeng.innerHTML = "data penginapan harus diisi";
+                                    return false;
+                                } else if (kota == "") {
+                                    alert("Kota Harus di Isi");
+                                    return false;
+                                } else if (namaPeng == "") {
+                                    alert("Nama Harus di Isi");
+                                    return false;
+                                } else if (tamu == "") {
+                                    alert("Jumlah Tamu Harus di Isi");
+                                    return false;
+                                } else if (Harga == "") {
+                                    alert("Harga Harus di Isi");
+                                    return false;
                                 }
+                            }
                         </script>
-                        
+
                     </div>
                     <!-- /.box-body -->
 
@@ -85,10 +84,8 @@
                     </div>
                 </form>
             </div>
-        
-    </div>
+
+        </div>
 </section>
 
 </div>
-
-
