da49bb507739aad2c9cef0ef3c18d96a83a7337b
KhosyAkmal <akmalkhosy@gmail.com>
2019-12-17 00:47:10 +0700

    edit model

--- application/models/Model_tempatTransport.php
+++ application/models/Model_tempatTransport.php
@@ -7,7 +7,6 @@ private $_table = "tempat_transport";
     public $nama_tempat;
     public $jenis_tempat;
     public $id_kota;
-    public $post_date;
 
 
     public function getAll()
@@ -23,14 +22,11 @@ private $_table = "tempat_transport";
     public function save()
     {
         $post = $this->input->post();
-        // $this->id_tempat = $post["id_tempat"];
-        $this->nama_transport = $post["nama_transport"];
+        $this->nama_tempat = $post["nama_tempat"];
         $this->jenis_tempat = $post["jenis_tempat"];
         $this->id_kota = $post["id_kota"];
-        $this->post_date = date('Y-m-d');
 
         $this->db->insert($this->_table, $this);
-        // var_dump($post);
     }
 
     public function update()
@@ -41,7 +37,6 @@ private $_table = "tempat_transport";
         $this->nama_transport = $post["nama_transport"];
         $this->jenis_tempat = $post["jenis_tempat"];
         $this->id_kota = $post["id_kota"];
-        $this->post_date = date('Y-m-d');
 
         $this->db->update($this->_table, $this, array("id_tempat" => $post["id_tempat"]));
     }
