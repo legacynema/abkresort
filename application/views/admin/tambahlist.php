8dbbe628db8bb3a86c8572e7aabcdcb2e6052ae3
dzikrialifa <dzikrialif99@gmail.com>
2019-12-16 22:44:08 +0700

    Add TambahList Page For Category

---
+++ application/views/admin/tambahlist.php
@@ -0,0 +1,90 @@
+<section class="content">
+      <div class="row">
+        <!-- left column -->
+        <div class="col-md-4">
+          <!-- general form elements -->
+          <div class="box box-primary">
+            <div class="box-header with-border">
+              <h3 class="box-title">Nama Tempat</h3>
+            </div>
+            <!-- /.box-header -->
+            <!-- form start -->
+            <form role="form" method="POST" action="">
+              <div class="box-body">
+                <div class="form-group">
+                  <label for="jenis_tempat_transport">Jenis Tempat Transport</label>
+                  <input type="text" class="form-control" id="jenis_tempat_transport" name="jenis_tempat_transport" placeholder="ex: Bandara">
+                </div>
+              </div>
+              <!-- /.box-body -->
+
+              <div class="box-footer">
+                <button type="submit" class="btn btn-primary"><i class="fa fa-send"></i> Simpan</button>
+                <button type="reset" class="btn btn-warning"><i class="fa fa-refresh"></i> Reset</button>
+              </div>
+            </form>
+          </div>
+          <!-- /.box -->
+
+        </div>
+
+        <div class="col-md-4">
+          <!-- general form elements -->
+          <div class="box box-primary">
+            <div class="box-header with-border">
+              <h3 class="box-title">Nama Transportasi</h3>
+            </div>
+            <!-- /.box-header -->
+            <!-- form start -->
+            <form role="form" method="POST" action="">
+              <div class="box-body">
+                <div class="form-group">
+                  <label for="jenis_transportasi">Jenis Transportasi</label>
+                  <input type="text" class="form-control" id="jenis_transportasi" name="jenis_transportasi" placeholder="ex: Pesawat">
+                </div>
+              </div>
+              <!-- /.box-body -->
+
+              <div class="box-footer">
+                <button type="submit" class="btn btn-primary"><i class="fa fa-send"></i> Simpan</button>
+                <button type="reset" class="btn btn-warning"><i class="fa fa-refresh"></i> Reset</button>
+              </div>
+            </form>
+          </div>
+          <!-- /.box -->
+
+        </div>
+
+        <div class="col-md-4">
+          <!-- general form elements -->
+          <div class="box box-primary">
+            <div class="box-header with-border">
+              <h3 class="box-title">Nama Detail Tempat Transport</h3>
+            </div>
+            <!-- /.box-header -->
+            <!-- form start -->
+            <form role="form" method="POST" action="">
+              <div class="box-body">
+                <div class="form-group">
+                  <label for="tempat_transport_detail">Tempat Transport</label>
+                  <input type="text" class="form-control" id="tempat_transport_detail" name="tempat_transport_detail" placeholder="ex: Bandara Juanda">
+                </div>
+              </div>
+              <!-- /.box-body -->
+
+              <div class="box-footer">
+                <button type="submit" class="btn btn-primary"><i class="fa fa-send"></i> Simpan</button>
+                <button type="reset" class="btn btn-warning"><i class="fa fa-refresh"></i> Reset</button>
+              </div>
+            </form>
+          </div>
+          <!-- /.box -->
+
+        </div>
+        <!--/.col (left) -->
+        
+        <!--/.col (right) -->
+      </div>
+      <!-- /.row -->
+    </section>
+</div>
